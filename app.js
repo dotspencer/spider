a70aa377ff8748579614c2faca5d704ebdf371eb
Spencer Smith <dontaskspencer@gmail.com>
2017-04-27 22:54:00 -0600

    Testing more urls

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ var request = require('request');
 var linker = require('./src/linker');
 var norm = require('./src/normalizer');
 
-var url = "http://www.utah.edu/";
+// var url = "http://www.utah.edu/";
 // var url = "http://dotspencer.com";
-request(url, receive);
+var urls = {
+  lds: "https://www.lds.org/?lang=eng",
+  utah: "http://www.utah.edu/",
+  liahona: "https://www.lds.org/liahona/2017/04/youth/personalize-your-gospel-study?cid=hp_fr_21-4-2017_dpfd_flhna_xlidyh-1_"
+}
+request(urls.liahona, receive);
 
 function receive(err, res, body){
   if(err) return console.error(err);
   // console.log(res.request.uri);
   var links = linker.getLinks(body);
   links = norm.normalize(res, links);
+
+  // for (var i = 0; i < links.length; i++) {
+  //   console.log(links[i]);
+  // }
   console.log(links);
 }
